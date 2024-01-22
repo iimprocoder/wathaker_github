82d996672ecbe6332eb61793e651143b7f64254c
alfaresof <alfaresof@gmail.com>
2024-01-22 20:58:02 +0300

    fix location

--- lib/models/json_connection.dart
+++ lib/models/json_connection.dart
@@ -1,33 +1,67 @@
 import 'dart:convert';
 import 'dart:async';
+import 'package:dio/dio.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:wathaker_application/models/prayer_data.dart';
 
 class JsonConnection {
   Data? list;
+  Future<Position> getCurrentUserLocation() async {
+    // we can compress it
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
 
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    final res = await Geolocator.getCurrentPosition();
+    return res;
+  }
   Future getPTLocation() async {
-    final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.medium);
-    double pLat = position.latitude;
-    double pLong = position.longitude;
-    final formattedDate = DateFormat("dd-MM-yyyy").format(DateTime.now());
-    int method = 4;
+    try {
 
-    final url =
-        "https://api.aladhan.com/v1/timings/$formattedDate?latitude=$pLat&longitude=$pLong&method=$method";
+      final position = await getCurrentUserLocation();
+      double pLat = position.latitude;
+      double pLong = position.longitude;
+      print(pLat);
+      print(pLong);
+      final formattedDate = DateFormat("dd-MM-yyyy").format(DateTime.now());
+      int method = 4;
 
-    http.Response res = await http.get(Uri.parse(url), headers: {
-      "Accept":
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-    });
+      final String url =
+          "https://api.aladhan.com/v1/timings/$formattedDate?latitude=$pLat&longitude=$pLong&method=$method";
+      Dio dio = Dio();
+      dio.interceptors.add(PrettyDioLogger());
+      final res = await dio.get(url);
+      // http.Response res = await http.get(Uri.parse(url), headers: {
+      //   "Accept":
+      //       "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+      // });
 
-    final data = json.decode(res.body);
+      final data = json.decode(res.data);
 
-    list = new Data.fromJson(data);
+      list = new Data.fromJson(data);
 
-    return list;
+      return list;
+    } catch (e) {
+      print(e);
+    }
   }
 }
